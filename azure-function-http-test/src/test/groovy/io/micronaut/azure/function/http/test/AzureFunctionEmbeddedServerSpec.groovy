ac512f3e3daeca2109af32a043427c16c9e4825c
Dean Wette <wetted@objectcomputing.com>
2022-07-26 16:00:38 -0500

    Revert "WIP: tests to check support for application/x-www-form-urlencoded MediaType"
    This reverts commit aa0f598dce16219a29d481d60b1560ec5c60d086.

--- azure-function-http-test/src/test/groovy/io/micronaut/azure/function/http/test/AzureFunctionEmbeddedServerSpec.groovy
+++ azure-function-http-test/src/test/groovy/io/micronaut/azure/function/http/test/AzureFunctionEmbeddedServerSpec.groovy
@@ -3,21 +3,18 @@ package io.micronaut.azure.function.http.test
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Consumes
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
-import spock.lang.Issue
-import spock.lang.PendingFeature
 import spock.lang.Specification
 
 import jakarta.inject.Inject
 
 @MicronautTest
-class AzureFunctionEmbeddedServerSpec extends Specification {
+class AzureFunRctionEmbeddedServerSpec extends Specification {
     @Inject
     @Client('/')
     HttpClient client
@@ -39,20 +36,6 @@ class AzureFunctionEmbeddedServerSpec extends Specification {
         result == 'goodbody'
     }
 
-    @PendingFeature
-    @Issue("https://github.com/micronaut-projects/micronaut-aws/issues/1410")
-    // this copies a test we have in micronaut-aws, MicronautLambdaHandlerSpec
-    // this is failing in AzureFunctionEmbeddedServer at line 261, with java.lang.IllegalStateException: STREAMED
-    // or is my test just wrong?
-    void 'test invoke form-url-encoded post via server'() {
-        when:
-        def result = client.toBlocking().retrieve(HttpRequest.POST('/api/test/nested-attribute', "message=World")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED), String)
-
-        then:
-        result == '{"message":"Hello World"}'
-    }
-
     @Controller('/test')
     static class TestController {
         @Get(value = '/', produces = MediaType.TEXT_PLAIN)
@@ -63,12 +46,6 @@ class AzureFunctionEmbeddedServerSpec extends Specification {
         @Post(value = '/', processes = MediaType.TEXT_PLAIN)
         String test(@Body String body) {
             return 'good' + body
-        }
-
-        @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-        @Post("/nested-attribute")
-        String save(@Body("message") String value) {
-            "{\"message\":\"Hello ${value}\"}";
         }
     }
 }
