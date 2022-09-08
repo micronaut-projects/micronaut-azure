17511e19d5366744945b33714e01736c3747d244
radovanradic <radicr@gmail.com>
2022-09-08 11:25:34 +0200

    Addressing new round of comments

--- azure-cosmos/src/test/groovy/io/micronaut/azure/cosmos/client/CosmosClientSpec.groovy
+++ azure-cosmos/src/test/groovy/io/micronaut/azure/cosmos/client/CosmosClientSpec.groovy
@@ -25,34 +25,33 @@ class CosmosClientSpec extends Specification implements AzureCosmosTestPropertie
 
     void "should get cosmos client and perform write/read operations"() {
         when:
-            CosmosClient client = context.getBean(CosmosClient)
-            CosmosDatabaseResponse databaseResponse = client.createDatabaseIfNotExists("testDb")
-            CosmosDatabase database = client.getDatabase(databaseResponse.getProperties().getId())
+        CosmosClient client = context.getBean(CosmosClient)
+        CosmosDatabaseResponse databaseResponse = client.createDatabaseIfNotExists("testDb")
+        CosmosDatabase database = client.getDatabase(databaseResponse.getProperties().getId())
 
-            CosmosContainerProperties containerProperties =
-                    new CosmosContainerProperties("person", "/name");
+        CosmosContainerProperties containerProperties = new CosmosContainerProperties("person", "/name");
 
-            CosmosContainerResponse containerResponse = database.createContainerIfNotExists(containerProperties);
-            CosmosContainer container = database.getContainer(containerResponse.getProperties().getId());
+        CosmosContainerResponse containerResponse = database.createContainerIfNotExists(containerProperties);
+        CosmosContainer container = database.getContainer(containerResponse.getProperties().getId());
 
-            Person person = new Person()
-            person.id = UUID.randomUUID().toString()
-            person.name = "Some Name"
+        Person person = new Person()
+        person.id = UUID.randomUUID().toString()
+        person.name = "Some Name"
 
-            CosmosItemRequestOptions cosmosItemRequestOptions = new CosmosItemRequestOptions();
-            def partitionKey = new PartitionKey(person.getName())
-            container.createItem(person, partitionKey, cosmosItemRequestOptions);
+        CosmosItemRequestOptions cosmosItemRequestOptions = new CosmosItemRequestOptions();
+        def partitionKey = new PartitionKey(person.getName())
+        container.createItem(person, partitionKey, cosmosItemRequestOptions);
 
-            CosmosItemResponse<Person> loadedItem = container.readItem(person.getId(), partitionKey, Person.class);
+        CosmosItemResponse<Person> loadedItem = container.readItem(person.getId(), partitionKey, Person.class);
 
-            def asyncClient = context.getBean(CosmosAsyncClient.class)
-            def asyncContainer = asyncClient.getDatabase(database.getId()).getContainer(containerResponse.getProperties().getId())
-            def asyncLoadedItem = asyncContainer.readItem(person.getId(), partitionKey, Person.class)
+        def asyncClient = context.getBean(CosmosAsyncClient.class)
+        def asyncContainer = asyncClient.getDatabase(database.getId()).getContainer(containerResponse.getProperties().getId())
+        def asyncLoadedItem = asyncContainer.readItem(person.getId(), partitionKey, Person.class)
 
         then:
-            loadedItem.getItem().id == person.id
-            loadedItem.getItem().name == person.name
+        loadedItem.getItem().id == person.id
+        loadedItem.getItem().name == person.name
 
-            asyncLoadedItem.block().getItem().name == person.name
+        asyncLoadedItem.block().getItem().name == person.name
     }
 }
