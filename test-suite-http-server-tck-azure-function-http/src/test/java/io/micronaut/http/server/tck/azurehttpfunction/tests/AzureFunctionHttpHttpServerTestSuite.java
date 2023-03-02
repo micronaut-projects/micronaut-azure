62ac98690560770b957b4fec59a0f41e337aba28
Tim Yates <tim.yates@gmail.com>
2023-03-02 11:01:25 +0000

    Retry without excluded tests

--- test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
+++ test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
@@ -8,19 +8,5 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Azure Functions")
-@ExcludeClassNamePatterns(
-    "io.micronaut.http.server.tck.tests.ErrorHandlerTest" +
-    "|io.micronaut.http.server.tck.tests.MiscTest" +
-    "|io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest" +
-    "|io.micronaut.http.server.tck.tests.VersionTest" +
-    "|io.micronaut.http.server.tck.tests.CookiesTest" +
-    "|io.micronaut.http.server.tck.tests.FilterErrorTest" +
-    "|io.micronaut.http.server.tck.tests.FiltersTest" +
-    "|io.micronaut.http.server.tck.tests.cors.SimpleRequestWithCorsNotEnabledTest" +
-    "|io.micronaut.http.server.tck.tests.OctetTest" +
-    "|io.micronaut.http.server.tck.tests.ParameterTest" +
-    "|io.micronaut.http.server.tck.tests.DeleteWithoutBodyTest" +
-    "|io.micronaut.http.server.tck.tests.filter.HttpServerFilterTest"
-)
 public class AzureFunctionHttpHttpServerTestSuite {
 }
