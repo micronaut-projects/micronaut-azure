145b84d398397aa16908413cc99fc09dd904b6e6
Sergio del Amo <sergio.delamo@softamo.com>
2023-05-26 10:42:16 +0200

    remove tests which now pass

--- test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
+++ test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
@@ -14,29 +14,19 @@ import org.junit.platform.suite.api.SuiteDisplayName;
     "io.micronaut.http.server.tck.tests.filter.RequestFilterTest", // Request is immutable
     "io.micronaut.http.server.tck.tests.filter.ResponseFilterTest", // Request is immutable
 
-    // Reactive issues
-    "io.micronaut.http.server.tck.tests.BodyTest", // Serialized reactive body is {"scanAvailable":true,"prefetch":-1}
-    "io.micronaut.http.server.tck.tests.FluxTest", // Serialized reactive body is {"scanAvailable":true,"prefetch":-1}
-
-    // Routing issues
-    "io.micronaut.http.server.tck.tests.VersionTest", // Matches more than one route
-    "io.micronaut.http.server.tck.tests.cors.CorsSimpleRequestTest", // Duplicate route exceptions
     "io.micronaut.http.server.tck.tests.cors.SimpleRequestWithCorsNotEnabledTest", // Duplicate route exceptions
-    "io.micronaut.http.server.tck.tests.MiscTest", // Matches more than one route and 'error' failure
-    "io.micronaut.http.server.tck.tests.cors.CorsDisabledByDefaultTest", // Duplicate route exceptions
 
     // Misc
-    "io.micronaut.http.server.tck.tests.FiltersTest", // Cors filter fails Method [OPTIONS] not allowed for URI [/filter-test/ok]. Allowed methods: [HEAD, GET]
     "io.micronaut.http.server.tck.tests.OctetTest", // Response is badly encoded (mime encoding?)
     "io.micronaut.http.server.tck.tests.ParameterTest", // ???
-
-    "io.micronaut.http.server.tck.tests.filter.ClientRequestFilterTest", // Multiple failures
-    "io.micronaut.http.server.tck.tests.filter.ClientResponseFilterTest", // multiple failures
 
     "io.micronaut.http.server.tck.tests.CookiesTest", // Cookies seem to not be found
     "io.micronaut.http.server.tck.tests.endpoints.health.HealthTest", // Response is OK, but with empty body
 
     "io.micronaut.http.server.tck.tests.HeadersTest", // headersAreCaseInsensitiveAsPerMessageHeadersSpecification
+    "io.micronaut.http.server.tck.tests.constraintshandler.ControllerConstraintHandlerTest" // Broken in servlet
+
+
 })
 public class AzureFunctionHttpHttpServerTestSuite {
 }
