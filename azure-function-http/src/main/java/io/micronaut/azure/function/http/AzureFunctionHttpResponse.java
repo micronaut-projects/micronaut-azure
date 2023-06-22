7604207099bcf5a24a897c02db50460298e16e04
Tim Yates <tim.yates@gmail.com>
2023-06-22 21:48:29 +0100

    Fix nativeResponse

--- azure-function-http/src/main/java/io/micronaut/azure/function/http/AzureFunctionHttpResponse.java
+++ azure-function-http/src/main/java/io/micronaut/azure/function/http/AzureFunctionHttpResponse.java
@@ -19,9 +19,11 @@ import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatusType;
 import io.micronaut.core.annotation.Internal;
 import io.micronaut.core.annotation.Nullable;
+import io.micronaut.core.convert.ArgumentConversionContext;
 import io.micronaut.core.convert.ConversionService;
 import io.micronaut.core.convert.value.MutableConvertibleValues;
 import io.micronaut.core.convert.value.MutableConvertibleValuesMap;
+import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.CaseInsensitiveMutableHttpHeaders;
 import io.micronaut.http.HttpHeaders;
 import io.micronaut.http.HttpStatus;
@@ -31,12 +33,21 @@ import io.micronaut.http.cookie.Cookie;
 import io.micronaut.http.netty.cookies.NettyCookie;
 import io.micronaut.servlet.http.ServletHttpResponse;
 import io.netty.handler.codec.http.cookie.ServerCookieEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * Azure implementation of {@link ServletHttpResponse}.
@@ -47,6 +58,9 @@ import java.util.Optional;
  */
 @Internal
 public final class AzureFunctionHttpResponse<B> implements ServletHttpResponse<HttpResponseMessage, B> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AzureFunctionHttpResponse.class);
+
     private final ByteArrayOutputStream body = new ByteArrayOutputStream();
     private int status = HttpStatus.OK.getCode();
     private  final MutableHttpHeaders headers;
@@ -55,7 +69,10 @@ public final class AzureFunctionHttpResponse<B> implements ServletHttpResponse<H
     private B bodyObject;
     private String reason = HttpStatus.OK.getReason();
 
-    public AzureFunctionHttpResponse(ConversionService conversionService, BinaryContentConfiguration binaryContentConfiguration) {
+    public AzureFunctionHttpResponse(
+        ConversionService conversionService,
+        BinaryContentConfiguration binaryContentConfiguration
+    ) {
         this.headers = new CaseInsensitiveMutableHttpHeaders(conversionService);
         this.binaryContentConfiguration = binaryContentConfiguration;
     }
@@ -134,28 +151,103 @@ public final class AzureFunctionHttpResponse<B> implements ServletHttpResponse<H
 
     @Override
     public HttpResponseMessage getNativeResponse() {
+        LOG.trace("Creating Azure Function HTTP Response");
+
         if (this.bodyObject instanceof HttpResponseMessage.Builder builder) {
+            LOG.trace("Using Azure Function HTTP Response Builder");
             return builder.build();
         }
-        return new HttpResponseMessage() {
-            @Override
-            public HttpStatusType getStatus() {
-                return com.microsoft.azure.functions.HttpStatus.valueOf(status);
+        HttpResponseMessage.Builder responseBuilder = new ResponseBuilder().status(com.microsoft.azure.functions.HttpStatus.valueOf(status));
+        getHeaders().forEach((s, strings) -> {
+            for (String string : strings) {
+                responseBuilder.header(s, string);
             }
+        });
+        if (bodyObject != null) {
+            if (binaryContentConfiguration.isBinary(getHeaders().getContentType().orElse(null))) {
+                responseBuilder.body(body.toByteArray());
+            } else {
+                responseBuilder.body(body.toString(getCharacterEncoding()));
+            }
+        }
+        return responseBuilder.build();
+    }
 
-            @Override
-            public String getHeader(String key) {
-                return headers.get(key);
-            }
+    private static class ResponseBuilder implements HttpResponseMessage.Builder, HttpResponseMessage, HttpHeaders {
+        private HttpStatusType status = com.microsoft.azure.functions.HttpStatus.OK;
+        private final Map<String, List<String>> headers = new LinkedHashMap<>(3);
+        private Object body;
 
-            @Override
-            public Object getBody() {
-                if (binaryContentConfiguration.isBinary(getHeaders().getContentType().orElse(null))) {
-                    return body.toByteArray();
-                } else {
-                    return body.toString(getCharacterEncoding());
-                }
+        @Override
+        public Builder status(HttpStatusType status) {
+            this.status = status;
+            return this;
+        }
+
+        @Override
+        public Builder header(String key, String value) {
+            headers.computeIfAbsent(key, (k) -> new ArrayList<>()).add(value);
+            return this;
+        }
+
+        @Override
+        public Builder body(Object body) {
+            this.body = body;
+            return this;
+        }
+
+        @Override
+        public HttpResponseMessage build() {
+            return this;
+        }
+
+        @Override
+        public HttpStatusType getStatus() {
+            return status;
+        }
+
+        @Override
+        public String getHeader(String key) {
+            List<String> v = headers.get(key);
+            if (CollectionUtils.isNotEmpty(v)) {
+                return v.iterator().next();
             }
-        };
+            return null;
+        }
+
+        @Override
+        public Object getBody() {
+            return this.body;
+        }
+
+        @Override
+        public List<String> getAll(CharSequence name) {
+            List<String> values = headers.get(name.toString());
+            if (values != null) {
+                return Collections.unmodifiableList(values);
+            }
+            return Collections.emptyList();
+        }
+
+        @Override
+        public String get(CharSequence name) {
+            return getHeader(name.toString());
+        }
+
+        @Override
+        public Set<String> names() {
+            return headers.keySet();
+        }
+
+        @Override
+        public Collection<List<String>> values() {
+            return headers.values();
+        }
+
+        @Override
+        public <T> Optional<T> get(CharSequence name, ArgumentConversionContext<T> conversionContext) {
+            return ConversionService.SHARED
+                .convert(name.toString(), conversionContext);
+        }
     }
 }
