6cea9141bb9b373ac3817a118509b87fda556e7f
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-22 18:03:08 +0200

    test: ignore TCK Test FilterProxyTest

--- test-suite-http-server-tck-azure-function-http-test/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpTestHttpServerTestSuite.java
+++ test-suite-http-server-tck-azure-function-http-test/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpTestHttpServerTestSuite.java
@@ -1,11 +1,13 @@
 package io.micronaut.http.server.tck.azurehttpfunction.tests;
 
+import org.junit.platform.suite.api.ExcludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SelectPackages("io.micronaut.http.server.tck.tests")
+@ExcludeClassNamePatterns("io.micronaut.http.server.tck.tests.FilterProxyTest")
 @SuiteDisplayName("HTTP Server TCK for Azure Functions")
 public class AzureFunctionHttpTestHttpServerTestSuite {
 }
