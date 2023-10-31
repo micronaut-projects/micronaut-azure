7ff8f1a4c1d25645c3aa07e75e046fe25e9fd017
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-31 12:53:01 +0100

    exclude tests in tck

--- test-suite-http-server-tck-azure-function-http-test/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpTestHttpServerTestSuite.java
+++ test-suite-http-server-tck-azure-function-http-test/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpTestHttpServerTestSuite.java
@@ -1,10 +1,17 @@
 package io.micronaut.http.server.tck.azurehttpfunction.tests;
 
+import org.junit.platform.suite.api.ExcludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
+@ExcludeClassNamePatterns({
+        "io.micronaut.http.server.tck.tests.RequestUriContainsQueryValueTest",
+        "io.micronaut.http.server.tck.tests.RequestUriTest",
+        "io.micronaut.http.server.tck.tests.FilterProxyTest",
+        "io.micronaut.http.server.tck.tests.HeadersTest"
+})
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Azure Functions")
 public class AzureFunctionHttpTestHttpServerTestSuite {
