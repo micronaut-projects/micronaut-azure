c083dc182799d42c5c5d802d9ce60d96ace1c3f0
Sergio del Amo <sergio.delamo@softamo.com>
2024-08-22 13:56:21 +0200

    tck: don’t exclude ResponseStatusTest (#732)
    * core 4.6.2
    
    * servlet 4.10.1
    
    * tck: don’t exclude ResponseStatusTest

--- test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
+++ test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
@@ -8,7 +8,6 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 @Suite
 @ExcludeClassNamePatterns({
         "io.micronaut.http.server.tck.tests.NoBodyResponseTest",
-        "io.micronaut.http.server.tck.tests.ResponseStatusTest",
         "io.micronaut.http.server.tck.tests.FilterProxyTest"
 })
 @SelectPackages("io.micronaut.http.server.tck.tests")
