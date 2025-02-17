1ab7fe37b5393277aaa5c372f6d0bb73840b6381
Sergio del Amo <sergio.delamo@softamo.com>
2025-02-17 16:16:21 +0100

    tck: sonViewsTest passes now

--- test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
+++ test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
@@ -5,7 +5,6 @@ import org.junit.platform.suite.api.*;
 @Suite
 @ExcludeClassNamePatterns({
         "io.micronaut.http.server.tck.tests.FilterProxyTest",
-        "io.micronaut.http.server.tck.tests.jsonview.JsonViewsTest",  // fails testJsonViewFlux
 })
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Azure Functions")
