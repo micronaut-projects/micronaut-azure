3495d20671b9d8c72c42f247b700262636c36103
Burt Beckwith <burt@burtbeckwith.com>
2025-06-11 10:27:24 -0400

    Adds the azure-tracing submodule, which allows publishing of application trace data to Azure Monitor

---
+++ azure-tracing/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("io.micronaut.build.internal.azure-module")
+}
+
+dependencies {
+    annotationProcessor(mn.micronaut.inject.java)
+    api(libs.opentelemetry.autoconfigure)
+    api(mnTracing.micronaut.tracing.opentelemetry)
+    testAnnotationProcessor(mn.micronaut.inject.java)
+    testCompileOnly(mn.micronaut.inject.groovy)
+}
---
+++ azure-tracing/src/main/java/io/micronaut/azure/tracing/AzureTracingConfigurationProperties.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.azure.tracing;
+
+import io.micronaut.context.annotation.BootstrapContextCompatible;
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.env.Environment;
+
+/**
+ * Configuration for Azure tracing.
+ *
+ * @since 5.10
+ */
+@ConfigurationProperties(AzureTracingConfigurationProperties.PREFIX)
+@BootstrapContextCompatible
+public class AzureTracingConfigurationProperties {
+
+    /**
+     * Prefix.
+     */
+    public static final String PREFIX = Environment.AZURE + ".tracing";
+
+    private String connectionString;
+
+    /**
+     * Set the connection string.
+     *
+     * @param connectionString the string
+     */
+    public void setConnectionString(String connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    /**
+     * Get the connection string.
+     *
+     * @return the string
+     */
+    public String getConnectionString() {
+        return connectionString;
+    }
+}
