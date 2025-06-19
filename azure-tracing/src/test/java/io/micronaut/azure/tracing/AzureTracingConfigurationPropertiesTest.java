65df17c756d2f073ee75c5affa3074363bbe2226
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-19 18:51:07 +0200

    test with context (#857)

--- azure-tracing/src/test/java/io/micronaut/azure/tracing/AzureTracingConfigurationPropertiesTest.java
+++ azure-tracing/src/test/java/io/micronaut/azure/tracing/AzureTracingConfigurationPropertiesTest.java
@@ -1,9 +1,26 @@
 package io.micronaut.azure.tracing;
 
+import io.micronaut.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AzureTracingConfigurationPropertiesTest {
+
+    @Test
+    void connectionStringCanBePopulatedViaConfiguration() {
+        Map<String, Object> config = Map.of(
+            "azure.tracing.connection-string", "foobar"
+        );
+        try (ApplicationContext ctx = ApplicationContext.run(config)) {
+
+            AzureTracingConfigurationProperties tracingConfig =
+                ctx.getBean(AzureTracingConfigurationProperties.class);
+            assertEquals("foobar", tracingConfig.getConnectionString());
+        }
+    }
 
     @Test
     void setterAndGetter() {
