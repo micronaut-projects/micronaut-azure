a5a61aafb48843f3ebe01b1507763fd45feefdab
issam liqali <liqaliissam@gmail.com>
2025-10-01 16:19:47 +0100

    try to fix cve

--- azure-function/build.gradle.kts
+++ azure-function/build.gradle.kts
@@ -8,4 +8,10 @@ dependencies {
     compileOnly(libs.managed.azure.functions.java.library)
     testCompileOnly(mn.micronaut.inject.groovy)
     testAnnotationProcessor(mn.micronaut.inject.java)
+
+    constraints {
+        implementation("io.projectreactor.netty:reactor-netty-http:1.2.8") {
+            because("Older versions have CVEs")
+        }
+    }
 }
