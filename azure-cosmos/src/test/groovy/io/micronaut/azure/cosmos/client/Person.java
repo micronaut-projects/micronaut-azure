e377a454c7e664da299fc06a6d304bea2e9a11c2
radovanradic <radicr@gmail.com>
2025-12-18 20:33:25 +0100

    Allow injection of custom item serializer

--- azure-cosmos/src/test/groovy/io/micronaut/azure/cosmos/client/Person.java
+++ azure-cosmos/src/test/groovy/io/micronaut/azure/cosmos/client/Person.java
@@ -1,34 +1,4 @@
 package io.micronaut.azure.cosmos.client;
 
-import java.util.UUID;
-
-public class Person {
-
-    private String id;
-    private String name;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Person{");
-        sb.append("id=").append(id);
-        sb.append(", name='").append(name).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
+public record Person(String id, String name) {
 }
