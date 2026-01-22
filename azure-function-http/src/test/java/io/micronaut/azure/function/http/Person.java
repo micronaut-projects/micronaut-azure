d17774ea68316d59dbfc8754004ddd0bc63e7113
issam liqali <liqaliissam@gmail.com>
2026-01-22 14:37:10 +0100

    fix serialization issue

--- azure-function-http/src/test/java/io/micronaut/azure/function/http/Person.java
+++ azure-function-http/src/test/java/io/micronaut/azure/function/http/Person.java
@@ -1,7 +1,11 @@
 package io.micronaut.azure.function.http;
 import io.micronaut.core.annotation.Creator;
 import io.micronaut.core.annotation.Introspected;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonPropertyOrder({"name", "age"})
 @Introspected
 public class Person {
     private String name;
@@ -11,8 +15,9 @@ public class Person {
         this.name = name;
     }
 
+    @JsonCreator
     @Creator
-    public Person(String name, int age) {
+    public Person(@JsonProperty("name") String name, @JsonProperty("age") int age) {
         this.name = name;
         this.age = age;
     }
