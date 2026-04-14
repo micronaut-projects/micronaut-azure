d17bfac257304b2e3da12205c6a6bcd1466e1723
yawkat <jonas.konrad@oracle.com>
2026-04-14 08:24:41 +0000

    Exclude FormBindingDeadlockTest from Azure HTTP TCK suite
    Co-Authored-By: multicode <multicode@yawk.at>

--- test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
+++ test-suite-http-server-tck-azure-function-http/src/test/java/io/micronaut/http/server/tck/azurehttpfunction/tests/AzureFunctionHttpHttpServerTestSuite.java
@@ -7,7 +7,8 @@ import org.junit.platform.suite.api.*;
         "io.micronaut.http.server.tck.tests.FilterProxyTest",
     "io.micronaut.http.server.tck.tests.ErrorHandlerFluxTest", // test fails testErrorHandlerWithFluxChunkedSignaledDelayedError
     "io.micronaut.http.server.tck.tests.forms.FormsJacksonAnnotationsTest", // test fails httpClientFormSubmissionsDoesNotSupportJacksonAnnotations"
-    "io.micronaut.http.server.tck.tests.forms.UploadTest"
+    "io.micronaut.http.server.tck.tests.forms.UploadTest",
+    "io.micronaut.http.server.tck.tests.forms.FormBindingDeadlockTest"
 })
 @SelectPackages("io.micronaut.http.server.tck.tests")
 @SuiteDisplayName("HTTP Server TCK for Azure Functions")
